c0e3ea7fe17af5e3870df2cbd287cdf4ace09609
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-17 17:16:50 +0300

    Update WorkshopMapLoader.cpp

--- WorkshopMapLoader.cpp
+++ WorkshopMapLoader.cpp
@@ -3,15 +3,7 @@
 BAKKESMOD_PLUGIN(WorkshopMapLoader, "Simple Test Plugin", "1.0", PLUGINTYPE_FREEPLAY)
 
 void WorkshopMapLoader::onLoad() {
-    if (!cvarManager) return;
-
-    cvarManager->log("Simple Test Plugin successfully loaded!");
-
-    cvarManager->registerNotifier("test_hello", [this](std::vector<std::string> args) {
-        if (cvarManager) {
-            cvarManager->log("Hello from the plugin console command!");
-        }
-    }, "Prints a confirmation message", PERMISSION_ALL);
+    cvarManager->log("Plugin loaded successfully!");
 }
 
 void WorkshopMapLoader::onUnload() {
