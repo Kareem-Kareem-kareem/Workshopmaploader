ba255288180726366cd59cadba2b059de20c5e82
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-17 16:54:04 +0300

    s

--- WorkshopMapLoader.h
+++ WorkshopMapLoader.h
@@ -1,12 +1,12 @@
 #pragma once
 
+#define WIN32_LEAN_AND_MEAN
 #include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/wrappers/GameObject/CVarManagerWrapper.h"
 #include <string>
 #include <vector>
 
-using namespace BakkesMod::Plugin;
-
-class WorkshopMapLoader : public BakkesModPlugin {
+class WorkshopMapLoader : public BakkesMod::Plugin::BakkesModPlugin {
 public:
     void onLoad() override;
     void onUnload() override;
