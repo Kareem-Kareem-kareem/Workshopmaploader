f455b483f2b885a29b6aa56dc8f6ba170438a57d
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-17 18:04:54 +0300

    Update WorkshopMapLoader.cpp

--- WorkshopMapLoader.cpp
+++ WorkshopMapLoader.cpp
@@ -4,6 +4,9 @@ BAKKESMOD_PLUGIN(WorkshopMapLoader, "test", "1.0", PLUGINTYPE_FREEPLAY)
 
 void WorkshopMapLoader::onLoad() {
     cvarManager->log("Plugin 'test' loaded successfully!");
+
+    // This creates the variable that the .set file refers to
+    cvarManager->registerCvar("test_enabled_var", "0", "Enables the test feature", true, true, 0, true, 1);
 }
 
 void WorkshopMapLoader::onUnload() {
