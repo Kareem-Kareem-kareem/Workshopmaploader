3481c150b6c017d868f56ec19e4267f4a006eeb0
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-17 16:22:44 +0300

    Update WorkshopMapLoader.h

--- WorkshopMapLoader.h
+++ WorkshopMapLoader.h
@@ -1,19 +1,20 @@
 #pragma once
+
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <memory>
 
 using namespace BakkesMod::Plugin;
 
-class WorkshopMapLoader : public BakkesModPlugin, public PluginWindow
-{
-private:
-    std::vector<std::wstring> mapFiles_;
-    std::string statusMsg_;
-    bool isWindowOpen_;
+struct WorkshopMap {
+    std::string name;
+    std::string path;
+};
 
+class WorkshopMapLoader : public BakkesModPlugin, public PluginWindow {
 public:
     void onLoad() override;
     void onUnload() override;
@@ -25,6 +26,17 @@ public:
     void OnOpen() override;
     void OnClose() override;
 
-    void RefreshMaps();
-    void LoadMap(const std::wstring& mapPath);
+    void ScanForMaps();
+    void EnterMap(const std::string& mapPath);
+    void CreateLanMatch();
+
+private:
+    std::vector<WorkshopMap> maps_;
+    int selectedMapIdx_ = -1;
+    bool isWindowOpen_ = false;
+    std::string statusMsg_ = "Ready";
+
+    char searchBuf_[256] = "";
+    char mapDirBuf_[512] = "";
+    char lanPasswordBuf_[128] = "";
 };
