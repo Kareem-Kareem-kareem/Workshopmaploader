f0c26aa5121306116ceaffffb6b64d080d91867e
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-17 16:30:16 +0300

    Update WorkshopMapLoader.h

--- WorkshopMapLoader.h
+++ WorkshopMapLoader.h
@@ -1,42 +1,19 @@
 #pragma once
 
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include "bakkesmod/plugin/pluginwindow.h"
 #include <string>
-#include <vector>
-#include <filesystem>
 #include <memory>
 
 using namespace BakkesMod::Plugin;
 
-struct WorkshopMap {
-    std::string name;
-    std::string path;
-};
-
-class WorkshopMapLoader : public BakkesModPlugin, public PluginWindow {
+class WorkshopMapLoader : public BakkesModPlugin {
 public:
     void onLoad() override;
     void onUnload() override;
 
-    void Render() override;
-    std::string GetPluginName() override;
-    void SetImGuiContext(uintptr_t ctx) override;
-
-    void OnOpen() override;
-    void OnClose() override;
-
-    void ScanForMaps();
-    void EnterMap(const std::string& mapPath);
-    void CreateLanMatch();
+    void OnPhysicsTick(std::string eventName);
+    void OnBallHit(std::string eventName);
 
 private:
-    std::vector<WorkshopMap> maps_;
-    int selectedMapIdx_ = -1;
-    bool isWindowOpen_ = false;
-    std::string statusMsg_ = "Ready";
-
-    char searchBuf_[256] = "";
-    char mapDirBuf_[512] = "";
-    char lanPasswordBuf_[128] = "";
+    bool isAutoAimActive_ = false;
 };
