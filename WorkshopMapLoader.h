af2d1577bb654a8f7069a840c0f8ec72ba884d7f
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-17 17:16:33 +0300

    Refactor WorkshopMapLoader header file

--- WorkshopMapLoader.h
+++ WorkshopMapLoader.h
@@ -1,10 +1,5 @@
 #pragma once
-
-#define WIN32_LEAN_AND_MEAN
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include "bakkesmod/wrappers/GameObject/CVarManagerWrapper.h"
-#include <string>
-#include <vector>
 
 class WorkshopMapLoader : public BakkesMod::Plugin::BakkesModPlugin {
 public:
